74716a752f61f253ea14085c762e174fb72af706
MWaqasAfridi <153609017+MWaqasAfridi@users.noreply.github.com>
2024-08-11 00:00:35 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+function anagram(words) {
+    return Object.values(
+      words.reduce((acc, word) => {
+        const sortedWord = word.split('').sort().join('');
+        if (!acc[sortedWord]) {
+          acc[sortedWord] = [];
+        }
+        acc[sortedWord].push(word);
+        return acc;
+    }, {})
+  );
+  }
+  console.log(anagram(['bat', 'tap', 'cat', 'tab', 'pat']));
 
-let array = ['bat', 'tab', 'cat', 'tab', 'path'];
-let sortedWord1 = array.toLowerCase().split("").sort().join("");
-let sortedWord2 = array.toLowerCase().split("").sort().join("");
-array.forEach((value) => {
-    if (sortedWord1 === sortedWord2) {
-return true
-    }
-    else{
-        return false
-    }
-    
-})
